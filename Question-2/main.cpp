9cf240be911f1646dc9230916c076d72eb55cee6
Ethan Leet <ethan@Ethans-MBP.modem>
2022-03-26 16:38:10 +1000

    question 2 cpp complete

--- Question-2/main.cpp
+++ Question-2/main.cpp
@@ -7,10 +7,10 @@
 using std::cout;
 using std::endl;
 
-double multi_app_simpson(double a, double b, int seg);
-double fn(double x);
-double rich_extrap(double x, double h);
-double central_dif(double x, double h);
+double multi_app_simpson(double a, double b, int seg, int t);
+double fn(double x, int t);
+double rich_extrap(double x, double h, int t);
+double central_dif(double x, double h, int t);
 
 int main()
 {
@@ -18,13 +18,13 @@ int main()
     std::ofstream outfile;
 
     for (int i = 1; i <= 30; i++)
-    { // 10 segments for 3 different t values  
+    { // 10 segments for 30 different t values  
         if (i <= 10) 
-            t1.push_back(multi_app_simpson(0, 10, i));
+            t1.push_back(multi_app_simpson(0, 10, i, i));
         else if (i <= 20)
-            t2.push_back(multi_app_simpson(10, 20, i));
+            t2.push_back(multi_app_simpson(10, 20, i, i));
         else
-            t3.push_back(multi_app_simpson(20, 30, i));
+            t3.push_back(multi_app_simpson(20, 30, i, i));
     }
 
     for (int i = 0; i < 10; i++) // evaluate distance for each segment
@@ -35,17 +35,13 @@ int main()
          << double(80500) / double(3) << endl;
 
     outfile.open("output.txt"); // used for creating graph
-    for (int i = 1; i <= 30; i++)
-    { // 10 segments for 3 different t values  
-        // remove cout
-        cout << "dv/dt at t = " << i << " = " << rich_extrap(double(i), 0.25) << endl;
-        outfile << rich_extrap(double(i), 0.25) << endl;
-    }
+    for (int i = 1; i <= 30; i++) // work out dv/dt
+        outfile << rich_extrap(i, 0.25, i) << endl;
 
     return 0;
 }
 
-double multi_app_simpson(double a, double b, int seg)
+double multi_app_simpson(double a, double b, int seg, int t)
 { // multi application simpsons 1/3
 
     double step = (b - a) / seg; // h
@@ -54,35 +50,35 @@ double multi_app_simpson(double a, double b, int seg)
     for (int i = 1; i <= seg - 1; i += 2)
     { // even sum part of formula
         double res = a + step * i;
-        odd_sum += fn(res);
+        odd_sum += fn(res, t);
     }
 
     for (int i = 2; i <= seg - 2; i += 2)
     { // odd sum part of formula
         double res = a + step * i;
-        even_sum += fn(res);
+        even_sum += fn(res, t);
     }
 
-    return (step / 3) * (fn(a) + 4 * odd_sum + 2 * even_sum + fn(b));
+    return (step / 3) * (fn(a, t) + 4 * odd_sum + 2 * even_sum + fn(b, t));
 }
 
-double fn(double x)
+double fn(double x, int t)
 {// apply applicable function for approriate t
-    if (x <= 10) 
+    if (t <= 10) 
         return 11 * (x * x) - 5 * x;
-    else if (x <= 20)
+    else if (t <= 20)
         return 1100 - 5 * x;
     else
         return 50 * x + 2 * ((x - 20) * (x - 20));
 }
 
-double rich_extrap(double x, double h)
+double rich_extrap(double x, double h, int t)
 { // richard extrapolation
 
     // fill in first column/row
     std::vector<std::vector<double>> matrix;
     matrix.push_back(std::vector<double>(1));
-    matrix[0][0] = central_dif(x, h);
+    matrix[0][0] = central_dif(x, h, t);
 
     // error tolerance to 6 significant figs (10^2-6)
     double err_tol = 0.5 * (1 / (10.0 * 10.0 * 10.0 * 10.0)), 
@@ -97,7 +93,7 @@ double rich_extrap(double x, double h)
         col++;
         h /= 2;
         matrix.push_back(std::vector<double>(row)); // allocate memory for the correct amount of columns
-        matrix[row][0] = (central_dif(x, h));       // add first column value
+        matrix[row][0] = (central_dif(x, h, t));       // add first column value
 
         int x_next = 4;
         for (int i = 1; i <= col; i++)
@@ -115,9 +111,9 @@ double rich_extrap(double x, double h)
         return matrix[row][col];
 }
 
-double central_dif(double x, double h)
+double central_dif(double x, double h, int t)
 {
     double fxh_plus = x + h;
     double fxh_minus = x - h;
-    return (fn(fxh_plus) - fn(fxh_minus)) / (2 * h);
+    return (fn(fxh_plus, t) - fn(fxh_minus, t)) / (2 * h);
 }
