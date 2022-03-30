3d06cd327c0bda5e0641cc2f5618dbb340bb7c00
Ethan Leet <ethan@Ethans-MBP.modem>
2022-03-30 18:45:38 +1000

    added append to fstream

--- Question-1/main.cpp
+++ Question-1/main.cpp
@@ -26,24 +26,26 @@ T optimal_h(T h, T upper, T inc);
 
 int main()
 {
-    cout << optimal_h<double>(0.001, 10, 0.001) << endl;
-    cout << optimal_h<float>(0.001, 10, 0.001) << endl;
-    half_float::half h(0.027), upper(3.99), inc(0.001);
+    cout << optimal_h<double>(0.001, 5, 0.001) << endl;
+    cout << optimal_h<float>(0.001, 5, 0.001) << endl;
+    half_float::half h(0.001), upper(3), inc(0.001);
     cout << optimal_h<half_float::half>(h, upper, inc) << endl;
     return 0;
 }
+
 template <typename T>
 T optimal_h(T h, T upper, T inc)
 {// compare double vs float
     std::ofstream outfile;
-    outfile.open("outfile.txt"); // used for creating graph of derivative
-
+    outfile.open("outfile.txt", std::ofstream::app); // used for creating graph of derivative
     T actual = fx_<T>(T(0.5)), error = T(1.), best_h = T(0.);
+
     for (T i = h; i <= upper; i += inc)
     {// loop 10k different h values
         T predicted = central_dif<T>(T(0.5), i);
         T true_error = fabs(log10(actual - predicted));
         outfile << true_error << endl;
+
         if (true_error < error) 
         {// update current best h
             error = true_error;
